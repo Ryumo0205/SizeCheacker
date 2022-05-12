a75737379ec283d1417e627a6c3bad48d9fd1924
Sigen_Chang <97452067+Ryumo0205@users.noreply.github.com>
2022-05-12 13:23:20 +0800

    test
    test

---
+++ GUI.py
@@ -0,0 +1,64 @@
+from tkinter.scrolledtext import ScrolledText
+import tkinter as tk
+from tkinter.ttk import Combobox
+from turtle import width
+
+
+
+# 定義方法_取得輸入欄內路徑
+
+
+def CHECK_IMG():
+    import sizechecker as SC
+    MULT_NUM = RADIOVALUE.get()  # 取得要檢查的倍數
+    SC.SizeCheck(MULT_NUM)  # 將倍數放入引數執行檢查
+    CHECK_TIMES = 0
+    for OUTPUT_FALSE in SC.FALSE_LIST:
+        MSG_SPACE.insert(tk.INSERT, SC.FALSE_LIST[CHECK_TIMES])
+        MSG_SPACE.insert(tk.INSERT, "\n")
+        CHECK_TIMES = CHECK_TIMES + 1
+    SC.FALSE_LIST = []  # 還原成空列表
+
+
+def CLEAR():
+    MSG_SPACE.delete("1.0", "end")
+
+
+#------------------介面內容-------------------#
+# 主視窗設定
+MAIN_WINDOW = tk.Tk()
+MAIN_WINDOW.geometry("500x500")
+MAIN_WINDOW.title("SizeChecker")
+# 創造可容納物件的框架
+FRAME = tk.Frame(MAIN_WINDOW, width=450, height=450)
+FRAME.pack(pady=20)  # 讓框架稍微遠離邊界
+# 把各部件放入 Frame
+WORD_1 = tk.Label(FRAME, text='圖片尺寸檢查器', font=(
+    "微軟正黑體", 18)).place(x=30, y=10)  # 第一行文字
+WORD_2 = tk.Label(FRAME, text="檢查下列路徑的圖片  支援格式 : jpg , png", font=(
+    "微軟正黑體", 10)).place(x=30, y=50)  # 第二行文字
+WORD_3 = tk.Label(FRAME, text="選擇想要檢查的倍數", font=(
+    "微軟正黑體", 10)).place(x=30, y=100)  # 倍數選擇文字
+WORD_4 = tk.Label(FRAME, text="下列圖片不符合倍數", font=(
+    "微軟正黑體", 12)).place(x=30, y=125)
+ENTRY_SPACE = tk.Entry(FRAME, width=55)  # 創造輸入欄
+ENTRY_SPACE.place(x=30, y=75)  # 輸入欄位置
+# 創造倍數選項
+RADIOVALUE = tk.IntVar()  # 倍數單選項內的變數型態
+MULTIPLE_NUM_1 = tk.Radiobutton(
+    FRAME, text="2倍", variable=RADIOVALUE, value=2)  # 倍數選單定義
+MULTIPLE_NUM_2 = tk.Radiobutton(FRAME, text="4倍", variable=RADIOVALUE, value=4)
+
+MULTIPLE_NUM_1.place(x=150, y=100)
+MULTIPLE_NUM_2.place(x=200, y=100)
+# 文字顯示欄位，用來顯示被檢查出有問題的圖片
+MSG_SPACE = ScrolledText(FRAME, width=53, height=20)
+MSG_SPACE.place(x=30, y=150)
+
+
+TEST_BTN = tk.Button(FRAME, text="Get",font=("微軟正黑體", 14), command=CHECK_IMG)
+TEST_BTN.place(x=300, y=100)
+TEST_BTN2 = tk.Button(FRAME, text="Clear",font=("微軟正黑體", 14), command=CLEAR)
+TEST_BTN2.place(x=370, y=100)
+
+MAIN_WINDOW.mainloop()
---
+++ sizechecker.py
@@ -0,0 +1,88 @@
+from ast import Pass
+import sys
+sys.path.append("modules")  # 新增根目錄下資料夾的模組搜尋路徑
+import GUI
+from pathlib import Path
+from PIL import Image
+
+""" 檢查資料夾路境內的圖片能否被設定的數字整除 """
+#PATH_STR = GUI.ENTRY_SPACE.get()
+#IMGS_PATH = list(Path(PATH_STR).glob("*"))  # 定義圖片路徑為列表
+FALSE_LIST = []  # 儲存不合倍數的空列表
+
+#------------列出所有要檢查的檔名(檢查用不向使用者開放)-----------------#
+
+def AllCheck():
+    PATH_STR = str()  #路徑字串
+    IMGS_PATH = list(Path(PATH_STR).glob("*"))  # 定義圖片路徑為列表
+    IMGS_NAME = []  # 儲存所有檔名的空列表
+    x = 0  # 用於紀錄取得路徑列表中的第幾個參數
+    for GET_NAME in IMGS_PATH:
+        PURENAME = Path(IMGS_PATH[x].stem)  # 取得不含路徑檔名
+        NAME = str(PURENAME)    # 將該路徑從路徑物件轉成字串
+        IMGS_NAME.append(NAME)  # 放至空列表
+        x = x + 1
+    for PRINT_ALL in IMGS_PATH:
+        print("圖片名稱:", PRINT_ALL)
+
+#------------檢查圖片是否符合倍數---------------#
+
+
+def SizeCheck(mult=int):
+    PATH_STR = GUI.ENTRY_SPACE.get()
+    IMGS_PATH_JPG = list(Path(PATH_STR).glob("*.jpg"))  # 定義圖片路徑為列表
+    IMGS_PATH_PNG = list(Path(PATH_STR).glob("*.png"))  # 定義圖片路徑為列表
+    print("檢查路徑:",PATH_STR)
+    SET_NUM = mult  # 設定除數
+    # 檢查JPG用迴圈
+    CHECK_TIMES_JPG = 0 #jpg檔檢查迴圈次數
+    CHECK_TIMES_PNG = 0 #png檔檢查迴圈次數
+    CHECK_BOOL_JPG = 0  #判斷jpg是否有不符合的檔案
+    CHECK_BOOL_PNG = 0  #判斷png是否有不符合的檔案
+    for CHECk_IMGS_JPG in IMGS_PATH_JPG:
+        IMG_NOW = Image.open(CHECk_IMGS_JPG)  # 開啟該迴圈的圖片
+        GETSIZE = IMG_NOW.size  # 取得圖片尺寸tuple放至變數
+        IMG_STATE = bool(True)  # 判斷是否被該倍數整除
+        for SizeCheck in GETSIZE:  # 迴圈判斷該尺寸tuple是否能被設定的值整除
+            if SizeCheck % SET_NUM == 0:
+                pass
+            else:
+                IMG_STATE = False
+                GET_FALSENAME = Path(IMGS_PATH_JPG[CHECK_TIMES_JPG].stem)  # 取得不含路徑檔名
+                FALSENAME = str(GET_FALSENAME)  # 路徑物件轉成字串
+                FALSE_LIST.append(FALSENAME + ".jpg")    # 加入不符合倍數的空列表
+                CHECK_BOOL_JPG = CHECK_BOOL_JPG + 1 #計算不符合的數量
+                break
+        CHECK_TIMES_JPG = CHECK_TIMES_JPG + 1   #換到下個路徑
+    if CHECK_BOOL_JPG == 0:
+        FALSE_LIST.append("★★★★★★★★JPG檔案全數通過★★★★★★★★")
+        FALSE_LIST.append("\n")
+    else:
+        FALSE_LIST.append("----------以上為有問題的JPG檔案-----------")
+        FALSE_LIST.append("\n")
+
+    for CHECk_IMGS_PNG in IMGS_PATH_PNG:
+        IMG_NOW = Image.open(CHECk_IMGS_PNG)  # 開啟該迴圈的圖片
+        GETSIZE = IMG_NOW.size  # 取得圖片尺寸tuple放至變數
+        IMG_STATE = bool(True)  # 判斷是否被該倍數整除
+        for SizeCheck in GETSIZE:  # 迴圈判斷該尺寸tuple是否能被設定的值整除
+            if SizeCheck % SET_NUM == 0:
+                pass
+            else:
+                IMG_STATE = False
+                GET_FALSENAME = Path(IMGS_PATH_PNG[CHECK_TIMES_PNG].stem)  # 取得不含路徑檔名
+                FALSENAME = str(GET_FALSENAME)  # 路徑物件轉成字串
+                FALSE_LIST.append(FALSENAME + ".png")    # 加入不符合倍數的空列表
+                CHECK_BOOL_PNG = CHECK_BOOL_PNG + 1 #計算不符合的數量
+                break
+        CHECK_TIMES_PNG = CHECK_TIMES_PNG + 1
+    if CHECK_BOOL_PNG == 0:
+        FALSE_LIST.append("★★★★★★★★PNG檔案全數通過★★★★★★★★")
+    else:
+        FALSE_LIST.append("----------以上為有問題的PNG檔案-----------")
+
+    print("---------不合", SET_NUM, "倍數的圖片-----------")
+
+    for PRINT_FALSE in FALSE_LIST:
+        print(PRINT_FALSE)
+    
